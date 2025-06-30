e881c98ec7ab5c2fcb0277cb3757069ee3128319
petmartino <pedromartinezdelpaso@gmail.com>
2025-06-30 00:14:30 -0600

    cambios

--- quotes/urls.py
+++ quotes/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     # This is the initial "Thank You" preview page
     path('cotizacion-recibida/<str:code>/', views.ver_cotizacion_view, name='ver_cotizacion'),
     # This is the full, trackable detail page with program and payment info
-    path('cotizacion/<str:code>/', views.ver_cotizacion_view, name='ver_cotizacion'),
+    #path('cotizacion/<str:code>/', views.cotizacion_detail_view, name='cotizacion_detail')
+    path('cotizacion/<str:code>/', views.ver_cotizacion_view, name='ver_cotizacion_view'),
     path('rastrear-cotizacion/', views.rastrear_cotizacion_view, name='rastrear_cotizacion'),
 ]
